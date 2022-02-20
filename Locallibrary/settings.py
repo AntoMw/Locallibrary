461675e3defacf92180b3cda58c47527398bf71f
AntonyMw <anthonnymw89@gmail.com>
2022-02-20 06:46:32 +0300

    defined library models.py
    added language model for books within the library

--- Locallibrary/settings.py
+++ Locallibrary/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2m&=(8wvnw*$lvbpk88gm&w$w-nr54!6zsib&@7-l*bi4@4fav'
+#_KEY = 'django-insecure-2m&=(8wvnw*$lvbpk88gm&w$w-nr54!6zsib&@7-l*bi4@4fav'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = []
 
