22f55c10f37eed393ccc36a80ea50c1def14a48f
AntonyMw <anthonnymw89@gmail.com>
2022-02-20 08:41:13 +0300

    1. Modify the view to generate counts for genres and books that contain a particular word (case insensitive), and pass the results to the context.
    2. Added count os instances w status reserves & Loaned

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
+
+from .models import Book, Author, BookInstance, Genre, Language
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'available')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    # Available books (status = 'on loan')
+    num_instances_loaned = BookInstance.objects.filter(status__exact='o').count()
+    # Available books (status = 'reserved')
+    num_instances_reserved = BookInstance.objects.filter(status__exact='r').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    #books with term hits
+    num_books_humans = Book.objects.filter(title__contains='Humans').count()
+
+    #politics genre
+    num_books_genre_politics = Book.objects.filter(genre__exact='1').count()
+
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_books_humans': num_books_humans,
+        'num_books_genre_politics': num_books_genre_politics,
+        'num_instances_available': num_instances_available,
+        'num_instances_loaned': num_instances_loaned,
+        'num_instances_reserved': num_instances_reserved,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
